4a9ef35e84858fae8da18d50a8b3e8f0161bf853
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-09-19 19:37:31 +0300

    connect the database, add the User entity

--- apps/api/src/user/entities/user.entity.ts
+++ apps/api/src/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
